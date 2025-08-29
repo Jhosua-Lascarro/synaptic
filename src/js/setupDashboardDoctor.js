9682896516b82ae0dfea731eae6db83f105c33f8
jose gomez <joseesapel@gmail.com>
2025-08-29 12:42:43 -0500

    Doctors'_view_styles_have_been_modified

--- src/js/setupDashboardDoctor.js
+++ src/js/setupDashboardDoctor.js
@@ -10,7 +10,7 @@ export function setupDashboardDoctor() {
   }, 0);
 }
 async function loadCitas() {
-  const content = document.getElementById("content-page");
+  const content = document.getElementById("content-citas");
   try {
     const res = await fetch("http://localhost:3000/appointments");
     const data = await res.json();
