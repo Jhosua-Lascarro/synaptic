987409e9ab9683bd3d3debec20c20c029029b421
Jhosua <jdlascarroa24@ul.edu.co>
2025-08-29 08:45:31 -0500

    redireccion a login

--- src/js/setupRegister.js
+++ src/js/setupRegister.js
@@ -1,44 +1,46 @@
 import { registerPost } from "@/auth/registerApi";
 
 export async function setupRegister() {
-    const form = document.getElementById("form");
+  const form = document.getElementById("form");
 
-    form.addEventListener("submit", async(e) => {
-        e.preventDefault();
-        try {
-        const fullname = document.getElementById("fullname").value;
-        const email = document.getElementById("email").value;
-        const identification = document.getElementById("identification").value;
-        const phone = document.getElementById("phone").value;
-        const birthdate = document.getElementById("birthdate").value;
-        const gender = document.querySelector('input[name="gender"]:checked')?.value;
-        const password = document.getElementById("password").value;
-        const confirm = document.getElementById("confirm-password").value;
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const fullname = document.getElementById("fullname").value;
+      const email = document.getElementById("email").value;
+      const identification = document.getElementById("identification").value;
+      const phone = document.getElementById("phone").value;
+      const birthdate = document.getElementById("birthdate").value;
+      const gender = document.querySelector(
+        'input[name="gender"]:checked'
+      )?.value;
+      const password = document.getElementById("password").value;
+      const confirm = document.getElementById("confirm-password").value;
 
-        if (password !== confirm) {
-            return alert("No coinciden las contraseñas")
+      if (password !== confirm) {
+        return alert("No coinciden las contraseñas");
+      }
+      const newuser = {
+        fullname: fullname,
+        emailInput: email,
+        identification: identification,
+        phone: phone,
+        birthdate: birthdate,
+        sexo: gender,
+        password_hash: password,
+        role: 3,
+      };
 
-        }
-        const newuser = {
-            fullname: fullname,
-            emailInput: email,
-            identification: identification,
-            phone: phone,
-            birthdate: birthdate,
-            sexo: gender,
-            password_hash: password,
-            role: 3
-        }
-
-
-
-        
-            const data = await registerPost(newuser);
-            if(!data.ok)
-            alert("Usuario creado exitosamente");
-        } catch (error) {
-            alert(error.response?.data?.message || error.message || "Error en el registro");
-        }
-    })
-
+      const data = await registerPost(newuser);
+      // if registration is successful, redirect to login
+      if (data) {
+        alert("Registro exitoso, por favor inicia sesión.");
+        window.location.href = "/";
+      }
+    } catch (error) {
+      alert(
+        error.response?.data?.message || error.message || "Error en el registro"
+      );
+    }
+  });
 }
