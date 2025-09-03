0dcf87c17a04249dcab9511fc1ef8f2e4254189b
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-03 10:52:39 -0500

    feat:update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,21 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss()],
-
-	// Root directory
-	root: path.resolve(__dirname, "src"),
-
-	// Alias configuration
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "src"),
-		},
-	},
-
-	// Server configuration
-	server: {
-		port: 5173,
-		open: true,
-	},
+  plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
