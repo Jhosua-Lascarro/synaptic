5982c9dfeda72274ebc95faf0dca0db61da80acb
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-03 10:30:23 -0500

    feat:the src folder is the root

--- frontend/src/auth/calendarApi.js
+++ frontend/src/auth/calendarApi.js
@@ -1,7 +1,16 @@
-const API_URL = "http://localhost:3000/appointments/";
+const API_URL = import.meta.env.VITE_API_URL // URL directa
 
 export async function getDateAppointmetns(fechaSeleccionada) {
-  const resp = await fetch(`${API_URL}fecha?date=${fechaSeleccionada} `);
-  const data = await resp.json();
-  return data;
+  try {
+    const resp = await fetch(`${API_URL}/appointments/fecha?date=${fechaSeleccionada}`);
+    if (!resp.ok) throw new Error(resp.statusText);
+    const data = await resp.json();
+    console.log("URL del backend:", API_URL);
+    return data;
+  } catch (err) {
+    console.error("Error al obtener citas:", err);
+    return [];
+  }
 }
+
+
